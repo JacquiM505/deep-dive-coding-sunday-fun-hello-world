7ee758ec0eb468aef438bfb7e7a871436ac82849
Jacqueline Martin <jacquimartin505@gmail.com>
2019-08-31 10:53:50 -0600

    add salutation to construct and return a salutation string,

--- src/edu/com/deepdive/prework/sunday/fun/HelloWorld.java
+++ src/edu/com/deepdive/prework/sunday/fun/HelloWorld.java
@@ -1,16 +1,18 @@
 package edu.com.deepdive.prework.sunday.fun;
 
-
 public class HelloWorld {
-    public static void main(String[] args) {
-      if (args.length > 0 ) {
-        salute((args.length > 0) ? args[0] : "World");
-      } else {
-        salute( "World");
-      }
-}
-private static void salute(String target) {
-  System.out.println("Hello" + target + "!");
-}
+
+ public static void main(String[] args) {
+   String greeting  =salutation(args);
+   salute(salutation(args));
+ }
+
+ private static String salutation(String[] args){
+   return "Hello, " + ((args.length > 0) ? args[0] : "World");
+ }
+
+ private static void salute(String salutation) {
+   System.out.println(salutation);
+ }
 
 }
